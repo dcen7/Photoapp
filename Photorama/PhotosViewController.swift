5a3e2a194aa3f50b92675b182f63b11fa367cd69
dcen7 <mdcengiz@gmail.com>
2022-03-17 15:26:57 -0500

    send the request fetch data and print the data

--- Photorama/PhotosViewController.swift
+++ Photorama/PhotosViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class PhotosViewController: UIViewController {
 
-    @IBOutlet private var imageView : UIImageView!
+    @IBOutlet private var imageView: UIImageView!
+    var store: PhotoStore!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        store.fetchInterestingPhotos()
     }
 
 
